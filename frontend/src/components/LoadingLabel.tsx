f274c3ede658f64bdd4a5d750b5d8515d45f577d
Adam Calleja <adampaulcalleja@gmail.com>
2026-03-26 16:29:49 +0000

    style change

--- frontend/src/components/LoadingLabel.tsx
+++ frontend/src/components/LoadingLabel.tsx
@@ -7,5 +7,9 @@ export function LoadingLabel({
 }) {
   const classes = className ? `loading-label ${className}` : "loading-label";
 
-  return <span className={classes}>{label}</span>;
+  return (
+    <span className={classes} data-text={label}>
+      {label}
+    </span>
+  );
 }
